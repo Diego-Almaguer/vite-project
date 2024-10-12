1de0d911bf3609066b56c3eded648cbac8354a78
Diego Almaguer <almagueralejandro66@gmail.com>
2024-10-12 13:29:05 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@
 import './App.css'
 
 import useMinisterio from './hooks/useMinisterio'
-import ListOfMinist from './components/listOfMinist'
-import MinisterioService from './services/ministerio';
+
 import Example  from './pages/home.jsx';
 
 function App() {
